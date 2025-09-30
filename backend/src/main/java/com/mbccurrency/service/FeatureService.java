16f82551a3f75882b72d61b67c063edcd30b5e28
JYERIMANG\jyldp <jyldpfla@naver.com>
2025-09-30 23:58:40 +0900

    1. 백엔드, db, frontend 연동 작업중

--- backend/src/main/java/com/mbccurrency/service/FeatureService.java
+++ backend/src/main/java/com/mbccurrency/service/FeatureService.java
@@ -2,7 +2,9 @@ package com.mbccurrency.service;
 
 import org.springframework.stereotype.Service;
 
+import com.mbccurrency.repository.PgExchangeRateRepository;
+
 @Service
 public class FeatureService {
-
+	private final PgFeatureRepository repo;
 }
