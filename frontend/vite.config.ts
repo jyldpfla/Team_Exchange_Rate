19e37f21195abda1886547af1dd3d66c781afc4b
JYERIMANG\jyldp <jyldpfla@naver.com>
2025-10-13 11:42:00 +0900

    1. frontend, backend 경로 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api' : {
-        target: 'http://152.67.200.168:8081',
+        target: 'http://140.245.76.205:8081',
         changeOrigin: true,
       }
     }
